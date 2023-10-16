2250f3390986ecee0f5403ae2c4d49f29ff7e65d
Deborah Lao <dlao7@my.bcit.ca>
2023-10-16 15:16:31 -0700

    Removes extraneous new Promise from readDir and second .then in main.js Promise chain.

--- main.js
+++ main.js
@@ -18,17 +18,11 @@ const pathProcessed = path.join(__dirname, "grayscaled");
 unzip(zipFilePath, pathUnzipped)
   .then(() => readDir(pathUnzipped))
   .then((pngList) => {
-    return new Promise((res, rej) => {
       Promise.all(
         pngList.map((png) =>
           grayScale(png, path.join(pathProcessed, path.basename(png)))
         )
       )
-        .then(() => {
-          console.log("All images done.");
-          res();
-        })
-        .catch((err) => rej(err));
-    });
+        .then(console.log("All images done."))
   })
   .catch((err) => console.log(err));
